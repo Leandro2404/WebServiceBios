9ddb56fff91e2f7dbcd8eac375817697cec730b3
Lean2404 <lillanes@excelenciadigital.net>
2022-12-29 21:41:55 -0300

    .

--- Modelos/Morden.php
+++ Modelos/Morden.php
@@ -213,7 +213,7 @@
 
        $headers = [
         'Content-type: application/json',
-        'Authorization: ' +$key,
+        'Authorization: '.$key,
        ];
 
         // use key 'http' even if you send the request to https://...
